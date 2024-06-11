9957b864a8f63d34b046fbeedde6b173440bb70a
26mar1982 <26mar1982@gmail.com>
2024-06-11 15:12:50 +0700

    Delete src/config.ts

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export const SITE: Site = {
-  website: "https://26mar1982.github.io/blog", // replace this with your deployed domain
-  author: "Lilis Saadah",
-  desc: "",
-  title: "Blog",
-  postPerPage: 3,
-};
