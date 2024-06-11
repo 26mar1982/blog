4ce509b5b476d9009a893c46181c4c8a57dcb69e
26mar1982 <26mar1982@gmail.com>
2024-06-11 15:06:00 +0700

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SITE: Site = {
+  website: "https://26mar1982.github.io/blog", // replace this with your deployed domain
+  author: "Lilis Saadah",
+  desc: "",
+  title: "Blog",
+  postPerPage: 3,
+};
